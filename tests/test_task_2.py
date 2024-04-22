638f384139404e4ec46e70b6131f9f5b7d559e32
Dmytro <126854340+Diemondos@users.noreply.github.com>
2024-04-22 14:24:30 +0300

    Update test_task_2.py

--- tests/test_task_2.py
+++ tests/test_task_2.py
@@ -12,7 +12,7 @@ def test_update_user():
         "email": "dienmondo@gmail.com"
         }
     })
-    response = requests.patch("https://favqs.com/api/users/:diemond", headers=headers, data=body)
+    response = requests.patch("https://favqs.com/api/users/diemond", headers=headers, data=body)
     print(response.text)
     print(response.status_code)
 
@@ -20,6 +20,6 @@ def test_login_email():
     headers = {'Authorization': 'Token token="122b1c2f61107698e65c53e405f572a5"',
                'User-Token': 'Y2wEDOOiyID6luAtxILWdvJGNDNY9I0Td0L16UeyqMlGUcZqd0K0wckHKe8QzlyqBWdsOVOdDDYYAC01/EyoIg==',
                'Content-Type': 'application/json'}
-    response = requests.get('https://favqs.com//api/users/:diemondo', headers=headers)
+    response = requests.get('https://favqs.com//api/users/diemondo', headers=headers)
     print(response.text)
     print(response.status_code)
